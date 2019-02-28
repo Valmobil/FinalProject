39a1277f95fab1ee4aa37b3db70b995e7b135ded
val <valmobil@gmail.com>
2019-02-28 08:57:57 +0200

    Initial project structure v1

---
+++ src/main/frontend/readme.txt
@@ -0,0 +1,2 @@
+This folder was created for Fronttend Part of our project.
+You can add there common rules + installation steps, etc
---
+++ src/test/java/ua/com/danit/MainTest.java
@@ -0,0 +1,11 @@
+package ua.com.danit;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest
+{
+    @Test
+    public static void Test_Example() {
+
+    }
+}
