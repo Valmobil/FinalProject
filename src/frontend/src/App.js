2fc813a9057f2264acaaea53159a644b634d7dc9
Alexei Leichenko <alekseyleychenko@gmail.com>
2019-02-28 22:17:20 +0200

    create-react-app (#1)

---
+++ src/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <p>
+            FINAL PROJECT "Switter"
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
