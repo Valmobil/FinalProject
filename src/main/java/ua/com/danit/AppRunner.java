a980e1bd6296ac5cef654b56db2ebfe5e4417222
olegzip <olegzip@ukr.net>
2019-03-02 12:08:17 +0200

    Spring Boot annotations added

--- src/main/java/ua/com/danit/AppRunner.java
+++ src/main/java/ua/com/danit/AppRunner.java
@@ -1,7 +1,12 @@
 package ua.com.danit;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AppRunner {
     public static void main(String[] args) {
-
+        SpringApplication.run(AppRunner.class, args);
     }
 }
