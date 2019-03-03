8e191cc3f57e3684152aef6d7c762b0fb1fc3e1e
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-03-03 19:30:08 +0200

    Redux added

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import './App.scss';
+import './App.css';
 import {Route, Switch } from "react-router-dom";
 import Auth from './Auth'
+import {connect} from 'react-redux'
 import Main from './components/Main/Main'
 import Login from './components/Login/Login'
 import Callback from './components/Callback/Callback'
@@ -9,24 +10,28 @@ import NoMatch from './components/NoMatch/NoMatch'
 
 const auth = new Auth();
 class App extends Component {
+
     render() {
         return (
             <div className="App">
-            <header className="App-header">
-  <Switch>
-     { auth.isAthenticated() ? <Route path = '/' render={(props) => <Main {...props} auth={auth}/>}
-     />
-      : <Route exact path = '/' render={(props) => <Login {...props} auth={auth}/>}
-     />}
-        <Route path = '/callback' render={(props) => <Callback {...props} auth={auth}/>}
-     />
-        <Route component={NoMatch}/>
-  </Switch>
-        </header>
-        </div>
-    );
+                <Switch>
+                    { auth.isAthenticated() ? <Route path = '/' render={(props) => <Main {...props} auth={auth}/>}
+                        />
+                        : <Route exact path = '/' render={(props) => <Login {...props} auth={auth}/>}
+                        />}
+                    <Route path = '/callback' render={(props) => <Callback {...props} auth={auth}/>}
+                    />
+                    <Route component={NoMatch}/>
+                </Switch>
+            </div>
+        );
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        users: state.users,
+    }
+}
 
-export default App;
+export default connect(mapStateToProps, null)(App);
--- src/frontend/src/components/Login/Login.js
+++ src/frontend/src/components/Login/Login.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, {Component} from 'react'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import orange from '@material-ui/core/colors/orange';
+import purple from '@material-ui/core/colors/purple';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import './Login.css'
 
-const Login = (props) => {
-    return(
-        <button onClick={props.auth.login}>Log in</button>
-    )
+
+class Login extends Component{
+    state = {
+        toggleLogin: 0,
+        login: '',
+        password: '',
+        confirmPassword: '',
+    };
+
+    handleLoginChange = (event, toggleLogin) => {
+        this.setState({ toggleLogin });
+    };
+
+    handleInput = (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+    render() {
+        const { login, password, confirmPassword, toggleLogin} = this.state;
+        return (
+            <div className="login-container">
+                <MuiThemeProvider theme={theme}>
+                    <Tabs
+                        value={toggleLogin}
+                        onChange={this.handleLoginChange}
+                        indicatorColor="primary"
+                        textColor="primary"
+                        variant="scrollable"
+                        scrollButtons="auto"
+                    >
+                        <Tab label="Log in"/>
+                        <Tab label="Register"/>
+                    </Tabs>
+
+                    <Button style={style.button} onClick={this.props.auth.login}>Log in with Google</Button>
+                    <span>or</span>
+                    <TextField
+                        label="Login"
+                        id="mui-theme-provider-standard-input"
+                        style={style.input}
+                        name='login'
+                        value={login}
+                        onChange={this.handleInput}
+                    />
+                    <TextField
+                        label="Password"
+                        id="mui-theme-provider-standard-input"
+                        style={style.input}
+                        name='password'
+                        value={password}
+                        onChange={this.handleInput}
+                    />
+                    {this.state.toggleLogin === 1 &&
+                    <TextField
+                        label="Confirm password"
+                        id="mui-theme-provider-standard-input"
+                        style={style.input}
+                        name='confirmPassword'
+                        value={confirmPassword}
+                        onChange={this.handleInput}
+                    />
+                    }
+                    {((toggleLogin === 0 && login !== '' && password !== '') || (toggleLogin === 1 && login !== '' && password !== '' && password === confirmPassword))
+                    && <Button style={style.button}>Submit</Button>
+                    }
+                </MuiThemeProvider>
+            </div>
+        )
+    }
 }
-
+const theme = createMuiTheme({
+    palette: {
+        primary: orange,
+        secondary: purple,
+    },
+    typography: { useNextVariants: true },
+});
+const style={
+    input:{
+        width: '100%',
+        height: '50px',
+        color: '#fff',
+    },
+    button: {
+        color: '#ff9800',
+    },
+    submit:{
+        height: '30px',
+    }
+}
 export default Login
