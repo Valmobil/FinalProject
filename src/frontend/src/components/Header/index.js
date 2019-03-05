04f336341083313775ffd381b5dde6d44bb0c6b2
aleksey leichenko <alekseyleychenko@gmail.com>
2019-03-05 22:10:37 +0200

    resolve_conflict_stylelint2

--- src/frontend/src/components/Header/index.js
+++ src/frontend/src/components/Header/index.js
@@ -17,7 +17,7 @@ import MailIcon from '@material-ui/icons/Mail';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import deepPurple from '@material-ui/core/colors/deepPurple';
-import "./style.scss"
+
 
 const styles = theme => ({
   root: {
