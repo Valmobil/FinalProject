d291b9b24f0b44c3f2603853102f8c4d4c46d3ab
aleksey leichenko <alekseyleychenko@gmail.com>
2019-03-05 22:53:23 +0200

    .eslintcache added to ignore && fixed App.js_2

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './App.scss'
-
 import { Switch, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage/index'
 import WelcomePage from './pages/WelcomePage/index'
