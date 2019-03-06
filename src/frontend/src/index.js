87ed1841d86995168caff5bce6da2d27fee2095f
aleksey leichenko <alekseyleychenko@gmail.com>
2019-03-06 12:41:26 +0200

    eslint_style_fixed

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import App from './App'
 import { Provider } from 'react-redux'
-import store from "./store";
+import store from './store'
 import {BrowserRouter} from 'react-router-dom'
 // import * as serviceWorker from './serviceWorker';
-
-
-
 
 const app = (
   <Provider store={store}>
     <BrowserRouter>
-    <App/>
+      <App/>
     </BrowserRouter>
   </Provider>
 )
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(app, document.getElementById('root'))
 
 // serviceWorker.unregister();
