0072d377bb404937f06d2f9fe563313c936148cc
AndrewBubnov <38349955+AndrewBubnov@users.noreply.github.com>
2019-03-10 12:41:01 +0200

    First several page drafts added (#31)
    * Login/Main pages added
    
    * post request to back added, some minor improvements accomplished
    
    * sass fixed
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked
    
    * Updated modern stylelint config
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked
    
    * IntelliJ IDEA checked

--- src/frontend/src/actions/users.js
+++ src/frontend/src/actions/users.js
@@ -2,3 +2,10 @@ export const SET_AUTH = 'SET_AUTH'
 export const SET_USER = 'SET_USER'
 export const SET_ROLE = 'SET_ROLE'
 export const SET_SOCIAL_AUTH = 'SET_SOCIAL_AUTH'
+export const MENU_TOGGLE = 'MENU_TOGGLE'
+export const SET_CAR_LIST = 'SET_CAR_LIST'
+
+
+
+
+
--- src/frontend/src/store.js
+++ src/frontend/src/store.js
@@ -1,15 +1,16 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import index from './reducers/index'
-import thunk from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk';
 
-const middleware = [thunk]
+
+const middleware = [thunk];
 
 const store = createStore(
-  index,
-  compose(
-    applyMiddleware(...middleware)
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-)
+    rootReducer,
+    compose(
+        applyMiddleware(...middleware),
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
 
-export default store
+export default store;
