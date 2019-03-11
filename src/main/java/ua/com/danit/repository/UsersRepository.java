b5a26dcd3f53e31fcc8fce27716c865bc6825ff7
Valerii Trapeznikov <valmobil@gmail.com>
2019-03-11 08:52:58 +0200

    the 0103_Login_Register_Created api/users/login(<UserLogin>) entryPoints created (#30)
    * api/users/login(<UserLogin>) entryPoints created
    
    * api/users/login(<UserLogin>) entryPoints created - format correction
    
    * api/users/login(<UserLogin>) entryPoints created - Mobile Phone or Email login added
    
    * api/users/login(<UserLogin>) entryPoints created - Mobile Phone or Email login added - validated

--- src/main/java/ua/com/danit/repository/UsersRepository.java
+++ src/main/java/ua/com/danit/repository/UsersRepository.java
@@ -3,5 +3,11 @@ package ua.com.danit.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.danit.entity.User;
 
+import java.util.List;
+
 public interface UsersRepository extends JpaRepository<User, Long> {
+
+  List<User> findByUserPhone(String userPhone);
+
+  List<User> findByuserMail(String userMail);
 }
