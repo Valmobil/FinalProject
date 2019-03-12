5cf03c0c09e3ee42b5063206a3a2be7fbea4f1e6
aleksey leichenko <alekseyleychenko@gmail.com>
2019-03-12 13:01:19 +0200

    userProfile start page prototype

--- src/frontend/src/components/Header/Drawer/Drawer.js
+++ src/frontend/src/components/Header/Drawer/Drawer.js
@@ -1,43 +1,40 @@
-import React from 'react';
-import './Drawer.css';
-import {connect} from "react-redux";
-import { Link } from "react-router-dom";
-import { logOut, topMenuToggle } from "../../../actions/userCreators";
-
-
+import React from 'react'
+import './Drawer.css'
+import {connect} from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logOut, topMenuToggle } from '../../../actions/userCreators'
 
 const Drawer = (props) => {
-    const { auth, topMenuOpen } = props.users
-    const { topMenuToggle }= props
-    const classArray = topMenuOpen ? "drawer open" : "drawer";
-        return (
-            <div className={classArray}>
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                <Link to={'/main'} onClick={() => topMenuToggle(true)}>Main</Link>
-                <Link to={'/profile'} onClick={() => topMenuToggle(true)}>Profile</Link>
-                <Link to={'/'} onClick={() => {signOut(auth, props.logOut); topMenuToggle(true)}}>Sign out</Link>
-                </div>
-            </div>
-        )
-};
+  const { auth, topMenuOpen } = props.users
+  const { topMenuToggle } = props
+  const classArray = topMenuOpen ? 'drawer open' : 'drawer'
+  return (
+    <div className={classArray}>
+      <div style={{display: 'flex', flexDirection: 'column'}}>
+        <Link to={'/main'} onClick={() => topMenuToggle(true)}>Main</Link>
+        <Link to={'/profile'} onClick={() => topMenuToggle(true)}>Profile</Link>
+        <Link to={'/'} onClick={() => { signOut(auth, props.logOut); topMenuToggle(true) }}>Sign out</Link>
+      </div>
+    </div>
+  )
+}
 
 const signOut = (auth, logOut) => {
-    if (auth) auth.signOut();
-    logOut()
+  if (auth) auth.signOut()
+  logOut()
 }
 
 const mapStateToProps = (state) => {
-    return {
-        users: state.users,
-    }
+  return {
+    users: state.users
+  }
 }
 
 const mapDispatchToProps = (dispatch) => {
-    return {
-        logOut: () => dispatch(logOut()),
-        topMenuToggle: (topMenuOpen) => dispatch(topMenuToggle(topMenuOpen))
-    }
+  return {
+    logOut: () => dispatch(logOut()),
+    topMenuToggle: (topMenuOpen) => dispatch(topMenuToggle(topMenuOpen))
+  }
 }
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(Drawer);
+export default connect(mapStateToProps, mapDispatchToProps)(Drawer)
