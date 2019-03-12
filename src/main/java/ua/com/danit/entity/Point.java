a8aa0d88ae85a0e198d6210c3386310d41201d5e
olegzip <olegzip@ukr.net>
2019-03-13 00:20:29 +0200

    Point entity added

---
+++ src/main/java/ua/com/danit/entity/Point.java
@@ -0,0 +1,22 @@
+package ua.com.danit.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Value
+@Entity
+@Table(name = "points")
+@EqualsAndHashCode(callSuper = false)
+
+public class Point extends Auditable {
+  @Id
+  private long point_id;
+  private String point_name_en;
+  private String point_name_ru;
+  private String point_name_ua;
+}
+
