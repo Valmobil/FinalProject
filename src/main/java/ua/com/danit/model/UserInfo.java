5dbd5cb180a9bc07a0a2eacea849b649881b8100
Valerii Trapeznikov <valmobil@gmail.com>
2019-03-14 14:29:32 +0200

    01 11 change response format add car list and quick points (#44)
    * 01-11 Add UserCars and UserQuickPoints to User JSON
    
    * 01-11 Add UserCars and UserQuickPoints to User JSON with format correction
    
    * 01-11 Add UserCars and UserQuickPoints to User JSON with format correction
    
    * 01-11 Add UserCars and UserQuickPoints to User JSON with format correction 3

--- src/main/java/ua/com/danit/model/UserInfo.java
+++ src/main/java/ua/com/danit/model/UserInfo.java
@@ -1,11 +1,19 @@
 package ua.com.danit.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import ua.com.danit.entity.Car;
 import ua.com.danit.entity.User;
+import ua.com.danit.entity.UserPoint;
 
 import java.util.List;
 
-public class UserInfo extends User {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserInfo {
+  private User user;
   private List<Car> cars;
-//  private List<DestinationPoints>
+  private List<UserPoint> userPoints;
 }
