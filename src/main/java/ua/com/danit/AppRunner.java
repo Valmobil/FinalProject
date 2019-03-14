41a92d8df3a28809498f3d6163aefba4e7a94ba2
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-03-14 20:10:25 +0200

    receiving user data from DB added

--- src/main/java/ua/com/danit/AppRunner.java
+++ src/main/java/ua/com/danit/AppRunner.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories
 //@EnableJpaAuditin
 public class AppRunner {
-    public static void main(String[] args) {
-        SpringApplication.run(AppRunner.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(AppRunner.class, args);
+  }
 }
