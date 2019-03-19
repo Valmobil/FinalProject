8da43fe78e2d8870b6306485439ea82626d09146
olegzip <olegzip@ukr.net>
2019-03-19 15:59:58 +0200

    hardcoded Points added to data.sql chahges: Points entity, service, repo, contr

--- src/main/java/ua/com/danit/entity/Point.java
+++ src/main/java/ua/com/danit/entity/Point.java
@@ -1,20 +1,26 @@
 package ua.com.danit.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Value;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Value
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "points")
 @EqualsAndHashCode(callSuper = false)
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Point extends Auditable {
   @Id
   //@ManyToMany
-  //  @JoinTable(name="trip_point")
+  //@JoinTable(name="trip_point")
   private long pointId;
   private String pointNameEn;
   private String pointNameRu;
--- src/main/java/ua/com/danit/repository/PointsRepository.java
+++ src/main/java/ua/com/danit/repository/PointsRepository.java
@@ -2,10 +2,11 @@ package ua.com.danit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.com.danit.entity.Point;
-import ua.com.danit.entity.Trip;
-import ua.com.danit.entity.User;
 
 import java.util.List;
 
 public interface PointsRepository extends JpaRepository<Point, Long> {
+
+  List<Point> findPointByPointNameEnLike(String point_name_en);
+
 }
