90dfc3bea6e8e3122f703cd101185a1f343e4506
Oleg <tytarenkooleg@gmail.com>
2019-03-26 21:52:36 +0200

    update app.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             <ProtectedRoute exact path="/profile" component={Profile}/>
             <Route exact path="/map" component={Map}/>
             <Route exact path="/restore_password" component={PassRestoration}/>
-            <Route exact path="/C" component={ChangePassword} />
+            <Route exact path="/newpassword" component={ChangePassword} />
             <Route path='*' component={NoMatch}/>
           </Switch>
         </div>
