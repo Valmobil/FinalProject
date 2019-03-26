90d42b1a2aa10b740893d963d3586925c228d8a8
AndrewBubnov <andrew.bubnov75@gmail.com>
2019-03-26 11:47:52 +0200

    add public points to user point

--- src/frontend/src/actions/userCreators.js
+++ src/frontend/src/actions/userCreators.js
@@ -3,51 +3,51 @@ import { SET_AUTH, SET_USER, SET_CARS, SET_USER_POINTS, SET_COMMON_POINTS, SET_S
 import axios from 'axios'
 
 export const setAuthorization = (state) => dispatch => {
-  axios.post('/api/users/login', {
-    userLogin: state.login,
-    userPassword: state.password,
-    userToken: state.token})
-    .then(response => {
-      if (Object.keys(response.data).length !== 0) {
-        dispatch({type: SET_AUTH, payload: true})
-        dispatch({type: SET_USER, payload: response.data.user})
-        dispatch({type: SET_CARS, payload: response.data.cars})
-        dispatch(setUserPoints(response.data.userPoints))
-      } else {
-        dispatch(setLoginRejected(true))
-      }
-    })
-    .catch(err => console.log(err))
+    axios.post('/api/users/login', {
+        userLogin: state.login,
+        userPassword: state.password,
+        userToken: state.token})
+        .then(response => {
+            if (Object.keys(response.data).length !== 0) {
+                dispatch({type: SET_AUTH, payload: true})
+                dispatch({type: SET_USER, payload: response.data.user})
+                dispatch({type: SET_CARS, payload: response.data.cars})
+                dispatch(setUserPoints(response.data.userPoints))
+            } else {
+                dispatch(setLoginRejected(true))
+            }
+        })
+        .catch(err => console.log(err))
     axios.get('/api/points/filter/test')
         .then(res => dispatch({type: SET_COMMON_POINTS, payload: res.data}))
 }
 //* *********************
 
 export const setSocialAuth = (auth) => dispatch => {
-  dispatch({type: SET_SOCIAL_AUTH, payload: auth})
+    dispatch({type: SET_SOCIAL_AUTH, payload: auth})
 }
 //* **********************
 
 export const logOut = () => dispatch => {
-  dispatch({type: SET_AUTH, payload: false})
+    dispatch({type: SET_AUTH, payload: false})
 }
 
 //* **********************
 export const topMenuToggle = (topMenuOpen) => dispatch => {
-  dispatch({type: MENU_TOGGLE, payload: !topMenuOpen})
+    dispatch({type: MENU_TOGGLE, payload: !topMenuOpen})
 }
 
 //* **********************
 
 export const deleteCar = (carList, car) => dispatch => {
-  const newCarList = carList.filter(item => item !== car)
-  dispatch({type: SET_CAR_LIST, payload: newCarList})
+    const newCarList = carList.filter(item => item !== car)
+    dispatch({type: SET_CAR_LIST, payload: newCarList})
 }
 //* **********************
 
 export const addNewCar = (carList, car) => dispatch => {
-  const newCarList = [...carList, car]
-  dispatch({type: SET_CAR_LIST, payload: newCarList})
+    const newCarList = [...carList, car]
+    dispatch({type: SET_CAR_LIST, payload: newCarList})
 }
 //* **********************
 
@@ -62,14 +62,14 @@ export const setUserPoints = (payload) => dispatch => {
         url: '/api/userpoints',
         data: payload
     })
-    .catch(err => console.log(err))
+        .catch(err => console.log(err))
     dispatch({type: SET_USER_POINTS, payload})
 }
 //* **********************
 
 //from /profile
 export const setUserName = (name) => dispatch => {
-  dispatch({type: SET_USER_NAME, payload: name})
+    dispatch({type: SET_USER_NAME, payload: name})
 }
 //* **********************
 
