bdc47847397064df278061b59b649173ce701d97
val <valmobil@gmail.com>
2019-03-28 14:08:30 +0200

    02-10 BackEnd Send Email for Password restoration

--- src/main/java/ua/com/danit/controller/LoginController.java
+++ src/main/java/ua/com/danit/controller/LoginController.java
@@ -5,22 +5,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ua.com.danit.model.UserInfo;
 import ua.com.danit.model.UserLogin;
 import ua.com.danit.service.LoginsService;
+import ua.com.danit.service.PswdResetTokenService;
 import ua.com.danit.service.UsersService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("api/logins")
 public class LoginController {
   private UsersService usersService;
   private LoginsService loginsService;
+  private PswdResetTokenService pswdResetTokenService;
 
   @Autowired
-  public LoginController(UsersService usersService, LoginsService loginsService) {
+  public LoginController(UsersService usersService, LoginsService loginsService, PswdResetTokenService pswdResetTokenService) {
     this.usersService = usersService;
     this.loginsService = loginsService;
+    this.pswdResetTokenService = pswdResetTokenService;
   }
 
   //  @PostMapping("session")
@@ -46,8 +52,13 @@ public class LoginController {
     return loginsService.checkPasswordRestore(userLogin);
   }
 
+  @PostMapping("email")
+  public String checkUserByEmail(@RequestBody UserLogin userLogin, @RequestParam HttpServletRequest request) {
+    //https://www.baeldung.com/spring-security-registration-i-forgot-my-password
+    return pswdResetTokenService.checkUserByEmail(userLogin, request.getContextPath());
+  }
 
-  @GetMapping("test")
+    @GetMapping("test")
   public UserLogin showUserLoginFormat() {
     return loginsService.loginServiceTest();
   }
