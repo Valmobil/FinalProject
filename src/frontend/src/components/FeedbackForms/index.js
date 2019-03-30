9f848506a08daac31eec3ce7174de72a83fefabc
Oleg <tytarenkooleg@gmail.com>
2019-03-30 22:22:16 +0200

    add classnames modul

--- src/frontend/src/components/FeedbackForms/index.js
+++ src/frontend/src/components/FeedbackForms/index.js
@@ -1,192 +1,169 @@
-// import React, { Component} from 'react'
-// // const { createStore, bindActionCreators, applyMiddleware } = Redux;
-// // const { Provider, connect } = ReactRedux;
-// import './FeedbackForms.scss'
-//
-// export default class FeedbackForms extends Component {
-//
-//   constructor (props) {
-//     super(props)
-//     this.handleClickLike = this.handleClickLike.bind(this)
-//     this.handleClickDislike = this.handleClickDislike.bind(this)
-//     this.state = {
-//       counterLikes: props.likes,
-//       counterDislikes: props.dislikes,
-//       liked: false,
-//       disliked: false,
-//       total: props.likes + props.dislikes
-//     }
-//   }
-//
-//   handleClickLike = () => {
-//     if (!this.state.disliked) {
-//       this.setState({
-//         liked: !this.state.liked
-//       })
-//     } else {
-//       this.setState({
-//         liked: true,
-//         disliked: false,
-//         counterLikes: this.state.counterLikes + 1,
-//         counterDislikes: this.state.counterDislikes - 1,
-//         total: this.state.counterLikes + this.state.counterDislikes + 1
-//       })
-//     }
-//   }
-//
-//   handleClickDislike = () => {
-//     if (!this.state.liked) {
-//       this.setState({
-//         disliked: !this.state.dislike
-//       })
-//     } else {
-//       this.setState({
-//         liked: false,
-//         disliked: true,
-//         counterLikes: this.state.counterLikes - 1,
-//         counterDislikes: this.state.counterDislikes + 1,
-//         total: this.state.counterLikes + this.state.counterDislikes + 1
-//       })
-//     }
-//   }
-//   render () {
-//     return (
-//
-//       <div className="likeDislikeContainer">
-//
-//         <form>
-//
-//           <div className="likeDislike">
-//
-//             <span>
-//               {this.state.liked ? this.state.counterLikes : this.state.counterLikes}
-//             </span>
-//
-//             <button
-//               type="button"
-//               id={this.props.id}
-//               onClick={this.handleClickLike}
-//               value="likes"
-//             ><i className="far fa-thumbs-up"/></button>
-//
-//           </div>
-//
-//           <div className="likeDislike">
-//
-//             <span>
-//               {this.state.disliked ? this.state.counterDislikes : this.state.counterDislikes}
-//             </span>
-//             <button
-//               type="button"
-//               id={this.props.id}
-//               onClick={this.handleClickDislike}
-//               value="dislike"
-//             ><i className="far fa-thumbs-down"/></button>
-//           </div>
-//
-//         </form>
-//
-//       </div>
-//     )
-//   }
-// }
+import React, { Component } from 'react'
+import classnames from 'classnames'
+// import PropTypes from 'prop-types'
+// import { withStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardHeader from '@material-ui/core/CardHeader'
+import Avatar from '@material-ui/core/Avatar'
+import IconButton from '@material-ui/core/IconButton'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+// import CardActions from '@material-ui/core/CardActions'
+// import CardContent from '@material-ui/core/CardContent'
+// import Button from '@material-ui/core/Button'
+// import Typography from '@material-ui/core/Typography'
 
-import React from 'react'
-import ClassNames from 'classnames'
 import './FeedbackForms.scss'
 
-class Button extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      // liked: false,
-      // disliked: false,
-      initLike: 0,
-      initDislike: 0
-    }
-
-    // this.onLikeClick = this.onLikeClick.bind(this)
-    // this.onDisLikeClick = this.onDisLikeClick.bind(this)
-  }
-
-  // onLikeClick () {
-  //   if (!this.state.disliked) {
-  //     this.setState({
-  //       liked: !this.state.liked + 1
-  //     })
-  //   } else {
-  //     this.setState({
-  //       liked: true,
-  //       disliked: false
-  //     })
+export default class Button extends Component {
+  // constructor (props) {
+  //   super(props)
+  //
+  //   this.state = {
+  //     // liked: false,
+  //     // disliked: false,
+  //     initLike: 0,
+  //     initDislike: 0
   //   }
+  //
+  //   // this.onLikeClick = this.onLikeClick.bind(this)
+  //   // this.onDisLikeClick = this.onDisLikeClick.bind(this)
   // }
-  handleClick () {
-    this.setState((prevState) => ({
-      initLike: prevState.initLike + 1
-    }))
-    this.setState((prevStateDis) => ({
-      initDislike: prevStateDis.initDislike + 1
-    }))
-  }
-
-  // onDisLikeClick () {
-  //   if (!this.state.liked) {
-  //     this.setState({
-  //       disliked: !this.state.disliked
-  //     })
-  //   } else {
-  //     this.setState({
-  //       liked: false,
-  //       disliked: true
-  //     })
-  //   }
+  //
+  // // onLikeClick () {
+  // //   if (!this.state.disliked) {
+  // //     this.setState({
+  // //       liked: !this.state.liked + 1
+  // //     })
+  // //   } else {
+  // //     this.setState({
+  // //       liked: true,
+  // //       disliked: false
+  // //     })
+  // //   }
+  // // }
+  // handleClick () {
+  //   this.setState((prevState) => ({
+  //     initLike: prevState.initLike + 1
+  //   }))
+  //   this.setState((prevStateDis) => ({
+  //     initDislike: prevStateDis.initDislike + 1
+  //   }))
   // }
+  //
+  // // onDisLikeClick () {
+  // //   if (!this.state.liked) {
+  // //     this.setState({
+  // //       disliked: !this.state.disliked
+  // //     })
+  // //   } else {
+  // //     this.setState({
+  // //       liked: false,
+  // //       disliked: true
+  // //     })
+  // //   }
+  // // }
+  state = { expanded: false };
 
   render () {
-    //   const classLikeButton = ClassNames({
-    //     'like-button': true,
-    //     'liked': this.state.liked
-    //   })
-    //
-    //   const classDisLikeButton = ClassNames({
-    //     'dislike-button': true,
-    //     'disliked': this.state.disliked
-    //   })
-
-    // console.log(this.state)
-    // console.log(classLikeButton)
-    // console.log(classDisLikeButton)
+    const { classes } = this.props
 
     return (
-      <div>
-        <button onClick={this.handleClick.bind(this)}><i className="fas fa-thumbs-up">|</i> {this.state.initLike}
-        </button>
+      <Card className={classes.card}>
+        <CardHeader
+          avatar={
+            <Avatar aria-label="Recipe" className={classes.avatar}>
+              R
+            </Avatar>
+          }
+          action={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          title="Shrimp and Chorizo Paella"
+          subheader="September 14, 2016"
+        />
+        <div>
+          <button onClick={this.handleClick.bind(this)}><i className="fas fa-thumbs-up">|</i> {this.state.initLike}
+          </button>
 
-        <button onClick={this.handleClick.bind(this)}><i
-          className="fas fa-thumbs-down">|</i> {this.state.initDislike} </button>
+          <button onClick={this.handleClick.bind(this)}><i
+            className="fas fa-thumbs-down">|</i> {this.state.initDislike} </button>
 
-      </div>
+        </div>
+      </Card>
 
-    // <div>
-    //   <span className={classLikeButton}
-    //     onClick={this.onLikeClick}>
-    //     <i className="fas fa-thumbs-up">|</i>
-    //     <span className="likes-counter">
-    //       {this.state.initLike + 1 }
-    //     </span>
-    //   </span>
-    //   <span className={classDisLikeButton}
-    //     onClick={this.onDisLikeClick}>
-    //     <i className="fas fa-thumbs-down">|</i>
-    //     <span className="dislikes-counter">
-    //       {this.state.disliked ? this.state.initDislike + 1 : this.state.initDislike}
-    //     </span>
-    //   </span>
-    // </div>
     )
   }
 }
 
-export default Button
+// import lllllll/
+
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import { withStyles } from '@material-ui/core/styles'
+// import Card from '@material-ui/core/Card'
+// import CardActions from '@material-ui/core/CardActions'
+// import CardContent from '@material-ui/core/CardContent'
+// import Button from '@material-ui/core/Button'
+// import Typography from '@material-ui/core/Typography'
+// import './FeedbackForms.scss'
+//
+// const styles = {
+//   card: {
+//     minWidth: 275
+//   },
+//   bullet: {
+//     display: 'inline-block',
+//     margin: '0 2px',
+//     transform: 'scale(0.8)'
+//   },
+//   title: {
+//     fontSize: 14
+//   },
+//   pos: {
+//     marginBottom: 12
+//   }
+// }
+// // handleClick = (param) => this.setState({[param]: this.state[param] + 1})
+//
+// function SimpleCard (props) {
+//   const { classes } = props
+//
+//   // const bull = <span className={classes.bullet}>•</span>
+//
+//   return (
+//     <Card className={classes.card}>
+//       <CardContent>
+//         {/* <Typography className={classes.title} color="textSecondary" gutterBottom> */}
+//         {/* Word of the Day */}
+//         {/* </Typography> */}
+//         {/* <Typography variant="h5" component="h2"> */}
+//         {/* be */}
+//         {/* {bull} */}
+//         {/* nev */}
+//         {/* {bull}o{bull} */}
+//         {/* lent */}
+//         {/* </Typography> */}
+//         {/* <Typography className={classes.pos} color="textSecondary"> */}
+//         {/* adjective */}
+//         {/* </Typography> */}
+//         {/* <Typography component="p"> */}
+//         {/* well meaning and kindly. */}
+//         {/* <br /> */}
+//         {/* {'"a benevolent smile"'} */}
+//         {/* </Typography> */}
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small" ><i className="fas fa-thumbs-up">|</i></Button>
+//         <Button size="small"><i className="fas fa-thumbs-down">|</i></Button>
+//       </CardActions>
+//     </Card>
+//   )
+// }
+//
+// SimpleCard.propTypes = {
+//   classes: PropTypes.object.isRequired
+// }
+//
+// export default withStyles(styles)(SimpleCard)
