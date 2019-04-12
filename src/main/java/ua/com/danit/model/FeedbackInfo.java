08c65e52a765bf21faa23b689e807abaf1301858
val <valmobil@gmail.com>
2019-04-12 11:42:39 +0300

    07-04-your-feedback-create-service-controller-repository

--- src/main/java/ua/com/danit/model/FeedbackInfo.java
+++ src/main/java/ua/com/danit/model/FeedbackInfo.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.web.ProjectedPayload;
 
 import javax.persistence.Column;
 import javax.persistence.ColumnResult;
@@ -14,32 +17,23 @@ import javax.persistence.NamedNativeQuery;
 import javax.persistence.SqlResultSetMapping;
 import java.text.SimpleDateFormat;
 
+//@Projection
+public interface FeedbackInfo {
+  String getUserName();
 
+  String getTripDateTime();
 
-@Data
-@AllArgsConstructor
-@Builder
-@NoArgsConstructor
-public class FeedbackInfo {
-  //  @Column(name="USER_NAME")
-  private String userName;
-  //  @Column(name="TRIP_DATE_TIME")
-  private SimpleDateFormat tripDateTime;
-  //  private String type;
-  //  @Column(name="NAME_FROM")
-  //  private String nameFrom;
-  //  @Column(name="NAME_TO")
-  //  private String nameTo;
-  //  @Column(name="FEEDBACK_DRIVER_TRIP_ID")
-  //  private Long feedbackDriverTripId;
-  //  @Column(name="FEEDBACK_PASSENGER_TRIP_ID")
-  //  private Long feedbackPassengerTripId;
-  //  @Column(name="FEEDBACK_USER_WHO_ID")
-  //  private Long feedbackUserWhoId;
-  //  @Column(name="FEEDBACK_USER_WHOM_ID")
-  //  private Long feedbackUserWhomId;
-  //  @Column(name="FEEDBACK_VALUE")
-  //  private int feedbackValue;
-  //  @Column(name="USER_PHOTO")
-  //  private String userPhoto;
+  String getUserType();
+
+  Long getFeedbackTripIdWho();
+
+  Long getFeedbackTripIdWhom();
+
+  String getUserPhoto();
+
+  String getTripPointNameFrom();
+
+  String getTripPointNameTo();
+
+  Integer getFeedbackValue();
 }
--- src/main/java/ua/com/danit/service/FeedbacksService.java
+++ src/main/java/ua/com/danit/service/FeedbacksService.java
@@ -3,29 +3,42 @@ package ua.com.danit.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.com.danit.entity.Feedback;
+import ua.com.danit.entity.User;
 import ua.com.danit.model.FeedbackInfo;
+import ua.com.danit.repository.FeedbacksRepository;
 import ua.com.danit.repository.UsersRepository;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Service
 public class FeedbacksService {
-//  private FeedbacksRepository feedbacksRepository;
-//  private UsersRepository usersRepository;
-//  private SecurirtyService securirtyService;
-//
-//  @Autowired
-//  FeedbacksService(FeedbacksRepository feedbacksRepository,
-//                   UsersRepository usersRepository,
-//                   SecurirtyService securirtyService) {
-//    this.feedbacksRepository = feedbacksRepository;
-//    this.usersRepository = usersRepository;
-//    this.securirtyService = securirtyService;
-//  }
-//
-//  //    public List<FeedbackInfo> askForNewListOfFeedbacks() {
-//  //      //    return feedbacksRepository.getSqlData();
-//  //      return new ArrayList<FeedbackInfo>();
-//  //    }
+
+  private FeedbacksRepository feedbacksRepository;
+  private UsersRepository usersRepository;
+
+  @Autowired
+  FeedbacksService(FeedbacksRepository feedbacksRepository,
+                   UsersRepository usersRepository) {
+    this.feedbacksRepository = feedbacksRepository;
+    this.usersRepository = usersRepository;
+  }
+
+  public List<FeedbackInfo> getForNewListOfFeedback(User user) {
+    return feedbacksRepository.getSqlData();
+  }
+
+  public List<FeedbackInfo> saveForNewListOfFeedback(List<Feedback> feedbaks, User userByAccessToken) {
+    //Do not save records without values (evaluation)
+
+    for (Iterator<Feedback> iterator = feedbaks.listIterator(); iterator.hasNext(); ) {
+      Feedback feedback = iterator.next();
+      if (feedback.getFeedbackValue() == 0) {
+        iterator.remove();
+      }
+    }
+    feedbacksRepository.saveAll(feedbaks);
+    return feedbacksRepository.getSqlData();
+  }
 }
