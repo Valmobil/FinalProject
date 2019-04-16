7ff6b7784fc99e7309c6ed5a2a198d60d6ba20dc
Valerii Trapeznikov <valmobil@gmail.com>
2019-04-16 20:55:06 +0300

    BackEnd - OpenStreet's points export module + new Points endPoint (#104)
    * 01-01 Login Authorization Token in Header
    
    * 11-15 Role select - Point End Point Change
    
    * BackEnd - OpenStreet's points export module + new Points endPoint
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check 2
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check 3

--- src/main/java/ua/com/danit/AppRunner.java
+++ src/main/java/ua/com/danit/AppRunner.java
@@ -1,16 +1,33 @@
 package ua.com.danit;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import ua.com.danit.service.XmlFileNewTreadService;
 
 
 @SpringBootApplication
 @EnableJpaRepositories
-//@EnableJpaAuditin
 public class AppRunner {
+
+  @Autowired
+  ApplicationContext ctx;
+
   public static void main(String[] args) {
-    SpringApplication.run(AppRunner.class, args);
+    ApplicationContext ctx;
+    ctx = SpringApplication.run(AppRunner.class, args);
+
+
+    //Start new tread for XML upload to DB
+    XmlFileNewTreadService readXml = (XmlFileNewTreadService) ctx.getBean("xmlFileNewTreadService");
+    readXml.setName("Thread 1");
+
+    readXml.start();
+
+
   }
+
 }
---
+++ src/main/java/ua/com/danit/service/XmlFilePointSaveCashService.java
@@ -0,0 +1,25 @@
+package ua.com.danit.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.com.danit.repository.PointsRepository;
+import ua.com.danit.entity.Point;
+
+@Service
+public class XmlFilePointSaveCashService {
+  private PointsRepository pointsRepository;
+
+
+
+  @Autowired
+  public XmlFilePointSaveCashService(PointsRepository pointsRepository) {
+    this.pointsRepository = pointsRepository;
+  }
+
+  public void savePointsToDb(Point pointNew) {
+    //    this.points.add(pointNew);
+    //    if (this.points.size() > stackQty) {
+    Point point = pointsRepository.save(pointNew);
+    //    }
+  }
+}
