67cfe312126547c5b6c16ec5208b6712d4540c46
Valerii Trapeznikov <valmobil@gmail.com>
2019-05-07 11:45:13 +0300

    Back End: refactor Trip Other + User Profile + Add AWS s3 Service (#131)
    * 01-01 Login Authorization Token in Header
    
    * 11-15 Role select - Point End Point Change
    
    * BackEnd - OpenStreet's points export module + new Points endPoint
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check 2
    
    * BackEnd - OpenStreet's points export module + new Points endPoint - Travis check 3
    
    * BackEnd - Image upload and download servise and endpoint created
    
    * BackEnd - External MAP points distinct + point English name generation
    
    * BackEnd - External MAP points distinct + point English name generation - Travis check
    
    * 0904 - Trip - select trip position
    
    * 0904 - Trip - select trip position - travis Check
    
    * BackEnd - Image save and restore
    
    * BackEnd - Image saving and providing
    
    * BackEnd - Image saving and providing
    
    * BackEnd - Image saving and providing v2
    
    * BackEnd - Preparation to Code Review
    
    * BackEnd - Preparation to Code Review v2
    
    * BackEnd - Clean Up End Points list after Code Review
    
    * BackEnd - Amazon S3 added
    
    * BackEnd - Facade layer added
    
    * BackEnd - Facade layer added for UserPoint
    
    * BackEnd - Issue with shorten path, Issue with crudService been initialization
    
    * BackEnd - facade, request, response for User Entity
    
    * BackEnd modelMapper Initialization
    
    * BackEnd - SignIn and SignUp refactoring
    
    * BackEnd - SignIn and SignUp refactoring v2
    
    * BackEnd - SignIn and SignUp refactoring v3 + Controller Mock test
    
    * BackEnd - Trip Controller refresh
    
    * BackEnd - Trip Controller refresh v2
    
    * BackEnd - Trip Controller & PasswordChange/Restore
    
    * BackEnd - Trip Controller & PasswordChange/Restore - Travis Check
    
    * BackEnd - AWS S3 setup
    
    * BackEnd - AWS S3 setup + Trip Correction v2
    
    * Back End: refactor Trip Other + User Profile + Add AWS s3 Service

--- src/main/java/ua/com/danit/controller/AmazonS3Controller.java
+++ src/main/java/ua/com/danit/controller/AmazonS3Controller.java
@@ -3,13 +3,18 @@ package ua.com.danit.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import ua.com.danit.dto.S3UploadResponse;
 import ua.com.danit.service.AmazonS3Service;
+import ua.com.danit.service.ImageService;
+import ua.com.danit.service.UserTokensService;
 
 import java.io.IOException;
 
@@ -17,16 +22,29 @@ import java.io.IOException;
 @RequestMapping("api/s3")
 public class AmazonS3Controller {
 
-  private AmazonS3Service s3Service;
+  //  private AmazonS3Service s3Service;
+  private ImageService imageService;
+  private UserTokensService userTokensService;
 
-  @Autowired
-  public AmazonS3Controller(AmazonS3Service s3Service) {
-    this.s3Service = s3Service;
+  //  @Autowired
+  //  public AmazonS3Controller(AmazonS3Service s3Service) {
+  //    this.s3Service = s3Service;
+  //  }
+  //
+  public AmazonS3Controller( ImageService imageService, UserTokensService userTokensService) {
+    //    this.s3Service = s3Service;
+    this.imageService = imageService;
+    this.userTokensService = userTokensService;
   }
+
+  //  @PostMapping("upload")
+  //  public ResponseEntity<S3UploadResponse> uploadImageToS3(@RequestBody MultipartFile imageFile) throws IOException {
+  //    return new ResponseEntity<>(s3Service.putImage(imageFile), HttpStatus.OK);
+  //  }
 
   @PostMapping("upload")
-  public ResponseEntity<S3UploadResponse> uploadImageToS3(@RequestBody MultipartFile imageFile) throws IOException {
-    return new ResponseEntity<>(s3Service.putImage(imageFile), HttpStatus.OK);
+  public String saveImageController(@RequestParam("fileUpload") byte[] file,
+                                    @RequestHeader String authorization) {
+    return imageService.saveNewImageToS3(file, userTokensService.findUserByAccessToken(authorization));
   }
-
 }
--- src/main/java/ua/com/danit/service/ImageService.java
+++ src/main/java/ua/com/danit/service/ImageService.java
@@ -5,18 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ua.com.danit.entity.Image;
 import ua.com.danit.entity.User;
+import ua.com.danit.error.KnownException;
 import ua.com.danit.repository.ImageRepository;
 
 @Service
 public class ImageService {
   private ImageRepository imageRepository;
   private UserTokensService userTokensService;
-
+  private AmazonS3Service amazonS3Service;
 
   @Autowired
-  public ImageService(ImageRepository imageRepository, UserTokensService userTokensService) {
+  public ImageService(ImageRepository imageRepository, UserTokensService userTokensService,
+                      AmazonS3Service amazonS3Service) {
     this.imageRepository = imageRepository;
     this.userTokensService = userTokensService;
+    this.amazonS3Service = amazonS3Service;
   }
 
   public byte[] getImageService(String imageId) {
@@ -29,7 +32,7 @@ public class ImageService {
 
   public String saveNewImage(byte[] file, User user) {
     if (user == null) {
-      return null;
+      throw new KnownException("Error! Cannot find user with such Access token!");
     }
     Image image = new Image();
     image.setUser(user);
@@ -37,5 +40,13 @@ public class ImageService {
 
     image = imageRepository.save(image);
     return user.getUserId().toString() + "_" + image.getImageId().toString();
+  }
+
+  public String saveNewImageToS3(byte[] file, User user) {
+    if (user == null) {
+      throw new KnownException("Error! Cannot find user with such Access token!");
+    }
+    byte[] fileDecoded = Base64.decodeBase64(new String(file).split(",")[1].getBytes());
+    return amazonS3Service.putImage(file);
   }
 }
