40e356c886ff71e6e1c41203a78d9d61246c6915
Valerii Trapeznikov <valmobil@gmail.com>
2019-05-28 16:13:50 +0300

    BackEnd: Change logic of passengers pickUp (#167)
    * BackEnd: Trip controller issues solved!
    
    * BackEnd: Trip controller issues solved - travis check!
    
    * BackEnd: Access token refresh during login + new trip lint!
    
    * BackEnd: eMail confirmation on BackEnd created!
    
    * BackEnd: New UserCar save
    
    * BackEnd: ImageProvider AWS Setup
    
    * BackEnd: UserProfile bug fixing
    
    * BackEnd: UserProfile bug fixing - travis check
    
    * BackEnd: User Car bug fixing
    
    * BackEnd: api/trips/other correction
    
    * BackEnd: Services code revise
    
    * BackEnd: Services code revise - Travis check
    
    * BackEnd: Change logic of passengers pickUp

--- src/frontend/src/actions/tripCreators.js
+++ src/frontend/src/actions/tripCreators.js
@@ -7,7 +7,7 @@ import {callApi} from "../utils/utils";
 
 export const setTrip = (trip) => dispatch => {
     console.log('setTrip: trip = ', trip)
-    callApi('put', '/api/trips', trip)
+    callApi('post', '/api/trips', trip)
         .then(res => {
           console.log('setTrip: res = ', res.data.tripId)
           dispatch({type: SET_MAIN_TRIP_ID, payload: res.data.tripId})
